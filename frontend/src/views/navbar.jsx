e39ce2d07dda28ea46785a89c232f64690277991
smitraval24 <smit24raval@gmail.com>
2023-04-14 16:40:37 +0530

    navbar update

--- frontend/src/views/navbar.jsx
+++ frontend/src/views/navbar.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 
-function Header(props){
-    return(<header>
-          <p className='company-logo'>Company</p>
-          <nav>
-            <ul>
-              <li><a onClick={()=> props.handleShow(false)}>Home</a></li>
-              <li><a href=''>Products</a></li>
-              <li><a href=''>Contact Us</a></li>
-              <li><a onClick={()=> props.handleShow(true)}>Cart<sup> {props.count} </sup></a></li>
-            </ul>
-            <a href='cart.html' className='cart-logo'>Cart</a>
-          </nav>
-        </header>);
+function Header(){
+    return(
+    <header>
+        this is the header 
+    </header>);
 }
 
 export default Header;
