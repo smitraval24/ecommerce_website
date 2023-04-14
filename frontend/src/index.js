1344b8beb38ebfa8a9ba71b09b40e9f83741aa55
smitraval24 <smit24raval@gmail.com>
2023-04-14 16:54:42 +0530

    routes configuration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import * as ReactDOMClient from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./views/login";
+import SignUp from "./views/signup";
+import AddProduct from "./views/addProduct";
+import Home from "./views/home";
+import PageNotFound from "./views/pageNotFound";
+import SignUpCustomer from "./views/signupCustomer";
+import SignUpSeller from "./views/signupSeller";
 
 const rootElement = document.getElementById('root');
 const root = ReactDOMClient.createRoot(rootElement);
@@ -12,6 +18,12 @@ root.render(
   <Router>
     <Routes>
       <Route path="/" element={<Login />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/addProduct" element={<AddProduct />}></Route>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/pagenotfound" element={<PageNotFound />}></Route>
+      <Route path="/signup-customer" element={<SignUpCustomer />}></Route>
+      <Route path="/signup-seller" element={<SignUpSeller />}></Route>
     </Routes>
   </Router>
   </React.StrictMode>
