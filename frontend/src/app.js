d278247998389d8ea2dab90a71695c89755daa5f
smitraval24 <smit24raval@gmail.com>
2023-04-16 09:41:23 +0530

    adding all routes from index to app file

---
+++ frontend/src/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./views/login";
+import SignUp from "./views/signup";
+import AddProduct from "./views/addProduct";
+import Home from "./views/home";
+import PageNotFound from "./views/pageNotFound";
+import SignUpCustomer from "./views/signupCustomer";
+import SignUpSeller from "./views/signupSeller";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+      <Routes>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/addProduct" element={<AddProduct />}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/pagenotfound" element={<PageNotFound />}></Route>
+      <Route path="/signup-customer" element={<SignUpCustomer />}></Route>
+      <Route path="/signup-seller" element={<SignUpSeller />}></Route>
+    </Routes>
+  </Router>
+    </div>
+  )
+}
+
+export default App;
