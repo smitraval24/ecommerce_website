8e0d786ff94d1ace6fd62373e9257049a4976003
smitraval24 <smit24raval@gmail.com>
2023-04-16 09:29:38 +0530

    adding classes for styling

--- frontend/src/views/login.jsx
+++ frontend/src/views/login.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import "./App.css"
 
 function Login() {
   return(
-    <Form action="/login" method="post">
-      <h1>Login</h1>
+    <Form action="/login" method="post" className='landingForm'>
+      <h1 className='heading'>Login</h1>
       <Form.Group className="mb-3" controlId="LoginEmail">
         <Form.Control type="email" placeholder="Email address" name="email" />
       </Form.Group>
@@ -15,7 +16,7 @@ function Login() {
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" className='btn'>
         Login
       </Button>
     </Form>
--- frontend/src/views/signup.jsx
+++ frontend/src/views/signup.jsx
@@ -2,32 +2,25 @@ import React from 'react';
 
 function SignUp() {
   return(
-  <div class="container">
-  <form class="login_form" action="/signup" method="post">
-    <h1 class="heading">SIGNUP</h1>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email address" name="email" />
+  <form className="landingForm" action="/signup" method="post">
+    <h1 className="heading">SIGNUP</h1>
+      <div className="mb-3">
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email address" name="email" />
       </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password" />
+      <div className="mb-3">
+        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" name="password" />
       </div>
-      <div class="mb-3">
-          <label for="exampleInputPassword2" class="form-label">Renter the password</label>
-          <input type="password" class="form-control" id="exampleInputPassword2w" placeholder="Renter the password" name="re_entered_password" />
+      <div className="mb-3">
+          <input type="password" className="form-control" id="exampleInputPassword2w" placeholder="Renter the password" name="re_entered_password" />
       </div>
-      <div class="mb-3">
-        <label for="role">Select your Role</label>
-      <select class="form-select" id="role" name="role">
-        <option>Customer</option>
+      <div className="mb-3">
+      <select className="form-select" id="role" name="role" required>w
         <option>Seller</option>
       </select>
       </div>
-      <button type="submit" class="btn btn-primary submit_button">SIGNUP</button>
+      <button type="submit" className="btn btn-primary submit_button">SIGNUP</button>
     </form>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-</div>
+
 )}
     
 export default SignUp;
