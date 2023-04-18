83272a6cc5ffe8eec4ad019e89b1501e1e67a123
smitraval24 <smit24raval@gmail.com>
2023-04-18 17:18:09 +0530

    adding productlist page

---
+++ frontend/src/views/productlist.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../app.css';
+
+function Productlist({product, addToCart}){
+    return(<div className='flex'>
+        {
+            product.map((productItem, productIndex)=>{
+                return (
+                    <div style={{width: '25%'}}>
+                        <div className='product-item'>
+                            <img src={productItem.url} width="25%"/>
+                            <p>{productItem.name} | {productItem.category}</p>
+                            <p>{productItem.seller}</p>
+                            <p> Rs. {productItem.price} /-</p>
+                            <button
+                            onClick={() => addToCart(productItem)}
+                            className="addtocart-btn" type="submit">Add to cart</button>
+                        </div>
+                    </div>
+                )
+            })
+        }
+    </div>);
+}
+
+export default Productlist;
