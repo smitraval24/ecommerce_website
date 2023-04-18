be06083d25aa43600b037722f152e023e8c32fd7
smitraval24 <smit24raval@gmail.com>
2023-04-18 17:19:18 +0530

    adding laningpage css file

--- frontend/src/views/login.jsx
+++ frontend/src/views/login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import "./App.css"
+import "./landingPage.css"
 
 function Login() {
   return(
