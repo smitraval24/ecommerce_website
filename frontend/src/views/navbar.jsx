f0941cc9e9f05fd9c917b16b2508bf8d7ddcbf69
smitraval24 <smit24raval@gmail.com>
2023-04-18 17:18:28 +0530

    navbar modification

--- frontend/src/views/navbar.jsx
+++ frontend/src/views/navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import '../index.css';
+import { NavLink } from 'react-router-dom';
 
-function Header(){
-    return(
-    <header>
-        this is the header 
-    </header>);
+function NavBar(props){
+    return(<header>
+          <p className='company-logo'>Company</p>
+          <nav>
+            <ul>
+              <li><a><NavLink to={"/"}>Home</NavLink></a></li>
+              <li><a><NavLink to={"/products"}>Products</NavLink></a></li>
+              <li><a><NavLink to={"/cart"}>Cart<sup> {props.count} </sup></NavLink></a></li>
+            </ul>
+            <a href='cart.html' className='cart-logo'>Cart</a>
+          </nav>
+        </header>);
 }
 
-export default Header;
+export default NavBar;
