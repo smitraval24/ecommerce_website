71827dda9c22d0a804e469e9edee7d5c6a1ad996
smitraval24 <smit24raval@gmail.com>
2023-04-18 17:18:45 +0530

    adding main file

---
+++ frontend/src/views/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../index.css';
+import Productlist from './productlist';
+
+function Main(){
+    return(
+        <main>
+          <section class="home-header-photo">
+            <div class="vertical-center">
+              <h1>Shopping</h1>
+              <h2>At Your Comfort</h2>
+            </div>
+          </section>
+        </main>
+    );
+}
+
+export default Main;
