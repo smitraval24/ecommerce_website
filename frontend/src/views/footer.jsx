d40a0ee81bf6a8b7898751a27c08ef4efaaf8004
smitraval24 <smit24raval@gmail.com>
2023-04-18 17:41:00 +0530

    updating links

--- frontend/src/views/footer.jsx
+++ frontend/src/views/footer.jsx
@@ -5,9 +5,9 @@ function Footer(){
     return(
     <footer>
           <ul className='footer-dev-links'>
-            <li><a href="">Om Patel</a></li>
-            <li><a href="">Smit Raval</a></li>
-            <li><a href="">Stavan Shah</a></li>
+            <li>Om Patel</li>
+            <li>Smit Raval</li>
+            <li>Stavan Shah</li>
           </ul>
           <ul className='footer-places'>
             <li>Gujarat</li>
