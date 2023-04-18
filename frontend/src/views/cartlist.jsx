bbc6335d8cf35ccf0af99e13fea0ae4ff6c8efef
smitraval24 <smit24raval@gmail.com>
2023-04-18 17:19:54 +0530

    adding cartlist page

---
+++ frontend/src/views/cartlist.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import "../app.css";
+
+function Cartlist({cart}){
+
+    const [ CART, setCART] = useState([])
+
+    useEffect(()=>{
+        setCART(cart)
+    },[cart])
+
+    return(
+        <div>
+            {
+                CART.map((cartItem, cartIndex) => {
+                    return(<div>
+                        <img src={cartItem.url} width={200}/>
+                        <span>{cartItem.name}</span>
+                        <button
+                            onClick={()=>{
+                                const CART2 = CART.map((item, index)=>{
+                                    return cartIndex === index ? {  ...item, quantity : item.quantity > 0 ? item.quantity - 1 : 0} : item 
+                                })
+                                setCART(CART2)
+                            }}
+                        >-</button>
+                        <span>{cartItem.quantity}</span>
+                        <button
+                            onClick={()=>{
+                                const CART2 = CART.map((item, index)=>{
+                                    return cartIndex === index ? {  ...item, quantity : item.quantity + 1} : item 
+                                })
+                                setCART(CART2)
+                            }}
+                        >+</button>
+                        <span>Rs. {cartItem.price * cartItem.quantity}</span>
+                    </div>)
+                })
+            }
+
+            <p>Total : Rs. 
+                {
+                    CART.map(item => item.price * item.quantity).reduce((total,  value) => total + value, 0)
+                }
+            </p>
+        </div>
+    )
+}
+
+export default Cartlist;
