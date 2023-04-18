2972535991464d5e03f9396344a9fc74b8db8f63
smitraval24 <smit24raval@gmail.com>
2023-04-18 17:19:43 +0530

    making footer

--- frontend/src/views/footer.jsx
+++ frontend/src/views/footer.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import '../index.css';
 
-function Footer() {
+function Footer(){
     return(
-        <footer>
-            e-commerce website
-        </footer>
-    )
+    <footer>
+          <ul className='footer-dev-links'>
+            <li><a href="">Om Patel</a></li>
+            <li><a href="">Smit Raval</a></li>
+            <li><a href="">Stavan Shah</a></li>
+          </ul>
+          <ul className='footer-places'>
+            <li>Gujarat</li>
+            <li>Ahmedabad</li>
+            <li>Gandhinagar</li>
+          </ul>
+    </footer>
+    );
 }
 
 export default Footer;
