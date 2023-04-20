42e03850dc3dd2b84d9848e649ba05eea854ebf1
smitraval24 <smit24raval@gmail.com>
2023-04-20 08:19:17 +0530

    adding Main component

--- frontend/src/views/home.jsx
+++ frontend/src/views/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './navbar';
 import Footer from './footer';
+import Main from './main';
 
 function Home(){
   return(
     <div>
       <NavBar/>  
+      <Main/>
       <Footer/>
     </div>
   )
